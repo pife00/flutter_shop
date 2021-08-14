0202defe9912745720ca0d02a557ab2f1b1cc924
pife00 <pife00@gmail.com>
2021-08-14 16:25:55 -0500

    195

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:shop/screens/product_details_screen.dart';
 import 'package:shop/screens/products_overview_screen.dart';
 
+import 'package:provider/provider.dart';
+import './providers/products.dart';
+
 void main(List<String> args) {
   runApp(MyApp());
 }
@@ -17,16 +20,19 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Colors.green[800],
-        scaffoldBackgroundColor: Colors.grey[50]
+    return ChangeNotifierProvider(
+      create: (context)=>Products(),
+      child: MaterialApp(
+        theme: ThemeData(
+          primaryColor: Colors.green[800],
+          scaffoldBackgroundColor: Colors.grey[50]
+        ),
+        title: 'My Shop',
+         home:ProductsOverViewScreen(),
+         routes: {
+           ProductDetailScreen.routeName: (context) =>ProductDetailScreen()
+         },
       ),
-      title: 'My Shop',
-       home:ProductsOverViewScreen(),
-       routes: {
-         ProductDetailScreen.routeName: (context) =>ProductDetailScreen()
-       },
     );
   }
 }
